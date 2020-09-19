6df7563fe6cacbc40d7a202ae2e652a4be35b060
clianor <ejrgus94160@gmail.com>
2020-09-19 14:11:33 +0900

    feat: Layout 작성
    - Wrapper 컴포넌트 작성
    - Layout 컴포넌트 작성
    
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { AppProps } from "next/app";
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { Layout } from "src/components/Layout";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
       <CSSReset />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 };
