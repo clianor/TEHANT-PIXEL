b3e44ccbe002c84ee75ca435858060718aa11bee
clianor <ejrgus94160@gmail.com>
2020-09-19 19:48:51 +0900

    refactor: MovieDetail 리팩토링
    - MovieDetail 컴포넌트로 분리
    
    Signed-off-by: clianor <ejrgus94160@gmail.com>

---
+++ src/components/movie/MovieDetail.tsx
@@ -0,0 +1,54 @@
+import NextLink from "next/link";
+import { Box, Flex, Heading, Image, Link, Text } from "@chakra-ui/core";
+
+interface MovieDetailViewProps {
+  large_cover_image: string;
+  title_long: string;
+  rating: string;
+  like_count: string;
+  download_count: string;
+  description_full: string;
+}
+
+const MovieDetailView: React.FC<MovieDetailViewProps> = ({
+  large_cover_image,
+  title_long,
+  rating,
+  like_count,
+  download_count,
+  description_full,
+}) => {
+  return (
+    <Flex flexDirection="column">
+      <Flex flexDirection={{ xs: "column", md: "row" }} pb="1.5rem">
+        <Box minWidth="200px" margin="auto">
+          <Image src={large_cover_image} alt={title_long} />
+        </Box>
+        <Box flex="3" pl={{ md: 5 }}>
+          <Flex justifyContent="space-between" alignItems="center" mb="5">
+            <Heading size="lg">{title_long}</Heading>
+            <Text>{rating}</Text>
+          </Flex>
+          <Text>Like: {like_count}</Text>
+          <Text mb="5">Download: {download_count}</Text>
+          <Text>{description_full}</Text>
+        </Box>
+      </Flex>
+      <Box>
+        <NextLink href="/movies">
+          <Link
+            _hover={{ color: "blue.500" }}
+            color="blue.200"
+            fontWeight="700"
+            display="block"
+            textAlign="left"
+          >
+            리스트로 돌아가기
+          </Link>
+        </NextLink>
+      </Box>
+    </Flex>
+  );
+};
+
+export default MovieDetailView;
--- src/pages/movie/[mid].tsx
+++ src/pages/movie/[mid].tsx
@@ -1,6 +1,5 @@
-import NextLink from "next/link";
-import { Box, Flex, Heading, Image, Link, Text } from "@chakra-ui/core";
 import axios, { AxiosRequestConfig } from "axios";
+import MovieDetailView from "src/components/movie/MovieDetail";
 
 interface MovieDetailProps {
   large_cover_image: string;
@@ -19,45 +18,15 @@ const MovieDetail: React.FC<MovieDetailProps> = ({
   download_count,
   description_full,
 }) => {
-  console.log(
-    large_cover_image,
-    title_long,
-    rating,
-    like_count,
-    download_count,
-    description_full
-  );
-
   return (
-    <Flex flexDirection="column">
-      <Flex flexDirection={{ xs: "column", md: "row" }} pb="1.5rem">
-        <Box minWidth="200px" margin="auto">
-          <Image src={large_cover_image} alt={title_long} />
-        </Box>
-        <Box flex="3" pl={{ md: 5 }}>
-          <Flex justifyContent="space-between" alignItems="center" mb="5">
-            <Heading size="lg">{title_long}</Heading>
-            <Text>{rating}</Text>
-          </Flex>
-          <Text>Like: {like_count}</Text>
-          <Text mb="5">Download: {download_count}</Text>
-          <Text>{description_full}</Text>
-        </Box>
-      </Flex>
-      <Box>
-        <NextLink href="/movies">
-          <Link
-            _hover={{ color: "blue.500" }}
-            color="blue.200"
-            fontWeight="700"
-            display="block"
-            textAlign="left"
-          >
-            리스트로 돌아가기
-          </Link>
-        </NextLink>
-      </Box>
-    </Flex>
+    <MovieDetailView
+      large_cover_image={large_cover_image}
+      title_long={title_long}
+      rating={rating}
+      like_count={like_count}
+      download_count={download_count}
+      description_full={description_full}
+    />
   );
 };
 
