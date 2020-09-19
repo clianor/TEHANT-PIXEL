cdb584f5158d7851827015c43a226b5a6c048322
clianor <ejrgus94160@gmail.com>
2020-09-19 21:42:36 +0900

    feat: Home 페이지 작성
    - Home 페이지 작성
    
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import { Box, Heading } from "@chakra-ui/core";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <Heading paddingBottom="5" size="lg">
+        사용된 라이브러리 및 프레임 워크
+      </Heading>
+      <ul>
+        <li>@chakra-ui/core: ^0.8.0</li>
+        <li>@emotion/core: ^10.0.35</li>
+        <li>@emotion/styled: ^10.0.27</li>
+        <li>axios: ^0.20.0</li>
+        <li>emotion-theming: ^10.0.27</li>
+        <li>next: 9.5.3</li>
+        <li>react: 16.13.1</li>
+        <li>react-dom: 16.13.1</li>
+        <li>@types/axios: ^0.14.0</li>
+        <li>@types/node: ^14.11.1</li>
+        <li>@types/react: ^16.9.49</li>
+        <li>husky: ^4.3.0</li>
+        <li>typescript: ^4.0.2</li>
+      </ul>
+    </Box>
+  );
 }
