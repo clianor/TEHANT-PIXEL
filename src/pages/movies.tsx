74a433d45d02fe719a80e4396d17380e826c47fe
clianor <ejrgus94160@gmail.com>
2020-09-19 21:26:20 +0900

    feat: 페이지네이션 구현
    - 페이지네이션 구현 (인피니티 스크롤링)
    - Throttling 구현
    
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -4,40 +4,49 @@ import { MovieView } from "../components/movie/MovieView";
 import { movie } from "../types/movie";
 import { MovieList } from "../components/movie/MovieList";
 
-export default function Movies() {
-  const [page, setPage] = useState(1);
-  const [movies, setMovies] = useState<[movie?]>([]);
+interface MoviesProps {
+  moviesProps: movie[];
+  pageProps: number;
+}
+
+const Movies: React.FC<MoviesProps> = ({ moviesProps, pageProps }) => {
+  const [page, setPage] = useState(pageProps);
+  const [movies, setMovies] = useState<movie[]>(moviesProps);
+  const [scroll, setScroll] = useState(0);
+
+  const getMovies = async () => {
+    const newMovies: movie[] = await getMoviesAPI(page);
+    setPage(page + 1);
+    setMovies([...movies, ...newMovies]);
+  };
+
+  const handleScroll = () => {
+    const scrollLocation = Math.round(
+      ((document.body.scrollTop || document.documentElement.scrollTop) /
+        (document.documentElement.scrollHeight -
+          document.documentElement.clientHeight)) *
+        100
+    );
+    setScroll(scrollLocation);
+  };
 
   useEffect(() => {
-    const options: AxiosRequestConfig = {
-      method: "GET",
-      url: `https://yts.mx/api/v2/list_movies.json?limit=4&page=${page}`,
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
-
-    axios(options).then((res) => {
-      console.log(res.data.data.movies);
-      const new_movies: [movie?] = res.data.data.movies.map((item: any) => ({
-        id: item.id,
-        title: item.title,
-        summary: item.summary,
-        background_image: item.medium_cover_image,
-        rating: item.rating,
-      }));
-
-      setMovies([...movies, ...new_movies]);
-      setPage(page + 1);
-    });
   }, []);
 
   useEffect(() => {
-    console.log(page, movies);
+    const timeoutId = setTimeout(() => {
+      if (scroll === 100) getMovies();
+    }, 200);
 
-    console.log(
-      movies.map((item: movie) => <MovieView key={item.id} movie={item} />)
-    );
-
-    console.log([<div key="1">test</div>, <div key="2">test2</div>]);
-  }, [movies]);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [scroll]);
 
   return (
     <MovieList>
@@ -46,4 +55,38 @@ export default function Movies() {
       ))}
     </MovieList>
   );
+};
+
+export async function getServerSideProps() {
+  const page = 1;
+
+  const movies = await getMoviesAPI(page);
+
+  return {
+    props: {
+      moviesProps: movies,
+      pageProps: page + 1,
+    },
+  };
 }
+
+const getMoviesAPI = async (page: number) => {
+  const options: AxiosRequestConfig = {
+    method: "GET",
+    url: `https://yts.mx/api/v2/list_movies.json?limit=4&page=${page}`,
+  };
+
+  const movies = await axios(options).then((res) =>
+    res.data.data.movies.map((item: any) => ({
+      id: item.id,
+      title: item.title,
+      summary: item.summary,
+      background_image: item.medium_cover_image,
+      rating: item.rating,
+    }))
+  );
+
+  return movies;
+};
+
+export default Movies;
